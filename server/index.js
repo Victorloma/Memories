5a63362034fd4618befccb5ca233b7b92a562f22
Victor Martinez <victorlorenzomartinez@gmail.com>
2022-08-31 17:12:45 +0200

    setting up front and backend for project

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import corse from 'corse';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose, { mongo } from "mongoose";
+import corse from "corse";
+
+
+const app = express();
+
+app.use(bodyParser.json({limit:"30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
+
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://DBuser:DBuser123@memories.i2zhak3.mongodb.net/?retryWrites=true&w=majority"
+
+const PORT = process.env.POR || 5000;
+
+// mongoose.connect(CONNECTION_URL, { useNewParser:})
