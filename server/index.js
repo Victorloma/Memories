49c3b8915a4b1f2b3674cb76ac1d4556610de345
Victor Martinez <victorlorenzomartinez@gmail.com>
2022-09-01 16:35:16 +0200

    added front and backend structure

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose, { mongo } from "mongoose";
-import corse from "corse";
+import mongoose from "mongoose";
+import cors from "cors";
+import {CONNECTION_URL} from "./env.js"
 
+import postRoutes from "./routes/posts.js"
 
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
 
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://DBuser:DBuser123@memories.i2zhak3.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.POR || 5000;
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=> app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((error)=>console.log(error.message));
 
-// mongoose.connect(CONNECTION_URL, { useNewParser:})
+mongoose.set("useFindAndModify", false);
