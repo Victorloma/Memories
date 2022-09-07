c39e5eb44184e31dde00c0beed6a122dbb1ca5c6
Victor Martinez <victorlorenzomartinez@gmail.com>
2022-09-07 16:33:15 +0200

    worked on memory form submit

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js"
 
 export const getPosts = async (req, res) => {
@@ -13,7 +14,7 @@ export const getPosts = async (req, res) => {
 
 export const createPost = async (req, res) => {
     const post = req.body;
-    
+
     const newPost = new PostMessage(post);
 
     try {
@@ -21,6 +22,17 @@ export const createPost = async (req, res) => {
 
         res.status(201).json(newPost)
     } catch (error) {
-        res.status(409).json({message: error.message})
+        res.status(409).json({ message: error.message })
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('no post with that id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
+
+    res.json(updatedPost);
 }
